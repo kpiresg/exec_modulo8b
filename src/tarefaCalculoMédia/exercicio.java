5c8b5dcc8f3257a56138851ac76d64ec60f58406
Kauan Pires <nauakpires@gmail.com>
2024-03-03 10:35:05 -0300

    First commit

---
+++ src/tarefaCalculoMédia/exercicio.java
@@ -0,0 +1,22 @@
+package tarefaCalculoMédia;
+
+public class exercicio {
+
+    public static void main(String args[]) {
+        calculaMedia();
+    }
+
+    public static void calculaMedia() {
+        int notaA1 = 4;
+        int notaA2 = 8;
+        int notaA3 = 2;
+        int notaA4 = 10;
+
+        int media = (notaA1 + notaA2 + notaA3 + notaA4) / 4;
+        System.out.println("A nota do aluno 1 é: " + notaA1);
+        System.out.println("A nota do aluno 2 é: " + notaA2);
+        System.out.println("A nota do aluno 3 é: " + notaA3);
+        System.out.println("A nota do aluno 4 é: " + notaA4);
+        System.out.println("A nota média foi: " + media);
+    }
+}
